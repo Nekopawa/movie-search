d2107d3219ef8366987a187f89baa5bb6731a0d8
Nekopawa <rafaalinehoffman@gmail.com>
2026-05-17 17:45:14 -0300

    Popular searches CSS

--- src/components/PopularSearches.jsx
+++ src/components/PopularSearches.jsx
@@ -1,9 +1,21 @@
 import "../styles/popularSearches.css";
 
 export default function PopularSearches() {
+    //temporary tags
+    const tags = ["cats", "animation", "anime"];
+
     return (
         <section id="popular-searches__container">
             <h2>Popular Searches</h2>
+            <ul>
+                {tags.map((tag) => {
+                    return (
+                        <li key={tag}>
+                            <p>{tag}</p>
+                        </li>
+                    );
+                })}
+            </ul>
         </section>
     );
 }
